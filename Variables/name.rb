f06a0202e135aa872f822d693a438dcaed8f8776
Edwin Elia <edwinselia@gmail.com>
2014-06-27 11:25:39 -0400

    Finish variables section

---
+++ Variables/name.rb
@@ -0,0 +1,20 @@
+puts "What is your name?"
+
+name = gets.chomp
+
+puts "Hello " + name
+
+
+10.times do
+  puts name
+end
+
+
+
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+puts "Hello " + first_name + " " + last_name
